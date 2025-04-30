ca4527ac92f6c633e0026107814db8b2af53e0d2
Sharath Kumar <sharathkumar3503@gmail.com>
2025-04-30 20:48:08 +0530

    dark mode to light mode

---
+++ dark.js
@@ -0,0 +1,11 @@
+const togglebutton=document.getElementById("togglemode");
+togglebutton.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-mode');
+});
+
+
+// const toggleButton = document.getElementById('toggleMode');
+
+// toggleButton.addEventListener('click', () => {
+//   document.body.classList.toggle('dark-mode');
+// });
